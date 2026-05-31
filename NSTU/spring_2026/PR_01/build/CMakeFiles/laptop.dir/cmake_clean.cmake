d75d78e050837c0dc8a4d821b0e5d4600f1d8e69
C0rtaz <crtaz59@gmail.com>
2026-05-31 12:08:13 +0700

    building

---
+++ NSTU/spring_2026/PR_01/build/CMakeFiles/laptop.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laptop.dir/laptop.c.o"
+  "CMakeFiles/laptop.dir/laptop.c.o.d"
+  "CMakeFiles/laptop.dir/main.c.o"
+  "CMakeFiles/laptop.dir/main.c.o.d"
+  "laptop"
+  "laptop.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/laptop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
